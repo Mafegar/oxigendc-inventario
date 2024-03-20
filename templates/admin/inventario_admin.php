50729c2dfcd4058dd35be3d5e45675d8239dc652
marcf <mafegar@jviladoms.cat>
2024-03-20 10:58:23 +0100

    adicion inventario_admin

--- templates/admin/inventario_admin.php
+++ templates/admin/inventario_admin.php
@@ -11,6 +11,10 @@
         exit();
     }
 
+    // Consulta a la base de datos para obtener el nombre de la persona que inicio sesion.
+    $sql = "SELECT * FROM usuarios WHERE username = '$username'";
+    $result = mysqli_query($conn, $sql);
+
 ?>
 
 
@@ -20,19 +24,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <!-- <link rel="stylesheet" href="./styles/inventario_admin.css"> -->
     <title>Inventario</title>
 
     <style>
         
-        html,body{
+        body{
             margin: 0;
             padding: 0;
             font-family: "Raleway", sans-serif;
-            /* background-image: url("../../img/medio_logo_abajo.png"); */
+            background-image: url("../../img/medio_logo_abajo.png");
             background-repeat: no-repeat;
             background-position: center bottom;
-            height: 100vh;
+            height: 97vh;
+            background-size: 1000px;
+            background-position-Y: 333px;
 
         }
 
@@ -87,6 +95,7 @@
         nav{
             height: 44vh;
             margin: auto;
+            width: 1300px;
         }
 
         nav ul{
@@ -106,11 +115,12 @@
             color: black;
         }
 
-        .inventario, .usuarios, .movimiento, .cerrar{
+        .inventario, .usuarios, .movimiento, .cerrar, .info-persona{
             display: flex;
             justify-content: center;
             align-items: center;
             border: 1px solid black;
+            border-radius: 10px;
             width: 200px;
             height: 50px;
         }
@@ -145,6 +155,22 @@
                 </div>
                 <div class="usuarios">
                     <li><a href="./crear_usuarios.php">Usuarios</a></li>
+                </div>
+            </div>
+            <div class="sect-3">
+                <div class="info-persona">
+                    <li>
+                        <?php 
+                            while($row = mysqli_fetch_assoc($result)){
+                                echo  $row['nombre'] . " " . $row['primer_apellido'] . " <br>";
+                                if($row['tipo_usuario'] == 1){
+                                    echo " Oficina";
+                                } else {
+                                    echo " Almacen";
+                                }
+                            }  
+                        ?>
+                    </li>
                 </div>
             </div>
             <div class="sect-2">
